712197ff6787d009d729bc787a2f3c8f978cc908
mephist0isaloser <111122065@nitt.edu>
2024-03-25 07:02:11 +0530

    version 1

--- cient1_prod.py
+++ cient1_prod.py
@@ -1,26 +1,26 @@
 import socket
 
-def send_message(server_addr, message, buffer_size=1024, timeout=2):
+def send_message(server_addr, message, buffer_size=4096, timeout=2):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.settimeout(timeout)
         seq_num = 0
-        message += '\n'  # Signify the end of the message
+        message += '\n'
 
-        # Sending each character of the message with a sequence number
-        for char in message:
-            data = f'{char}{seq_num:02}'.encode()
+        for i in range(0, len(message), buffer_size - 2):
+            chunk = message[i:i + buffer_size - 2]
+            data = f'{seq_num:02}'.encode() + chunk.encode()
+
             while True:
                 try:
                     sock.sendto(data, server_addr)
                     ack_data, _ = sock.recvfrom(buffer_size)
                     ack_seq_num = int(ack_data.decode().replace('ACK', ''))
                     if ack_seq_num == seq_num:
-                        break  # Move to the next character once acknowledgment is received
+                        break
                 except socket.timeout:
-                    continue  # Resend the character if timeout occurs
-            seq_num += 1  # Increment the sequence number for the next character
+                    continue
+            seq_num += 1
 
-        # Receiving and assembling the reversed message from the server
         reversed_message = receive_reversed_message(sock, seq_num, buffer_size, server_addr)
         print(f"Reversed message: '{reversed_message}'")
 
@@ -29,28 +29,28 @@ def receive_reversed_message(sock, start_seq_num, buffer_size, server_addr):
     seq_num = start_seq_num
 
     while True:
+        #print("Receiving data1")
         try:
             data, _ = sock.recvfrom(buffer_size)
-            if len(data) >= 3:  # Check for at least 1 char and 2 digits of seq_num
-                #print(data)
-                char, char_seq_num = data[:-2].decode(), int(data[-2:])
-                #print(char, char_seq_num, seq_num)
-                if True:  # Process only if seq_num matches expected
-                    #print(char)
-                    if char == '\n':  # Check for end-of-message character
-                        break  # Complete message received
-                    reversed_message += char  # Append the received character
-                    #print(reversed_message)
-                    #print(char)
+            if len(data) >= 3:
+                char_seq_num = int(data[:2])
+                chunk = data[2:].decode()
+                #print("Received data2: ", chunk, char_seq_num, seq_num)
+                if True:
+                    if chunk.endswith('\n'):
+                        reversed_message += chunk[:-1]  # Remove the last '\n'
+                        break
+                    #print("Received chunk3: ", chunk)
+                    reversed_message += chunk
                     ack = f'ACK{char_seq_num:02}'.encode()
-                    sock.sendto(ack, server_addr)  # Acknowledge the received character
-                    seq_num += 1  # Increment for the next expected character
+                    sock.sendto(ack, server_addr)
+                    seq_num += 1
         except socket.timeout:
-            break  # End the loop if no more data is received
+            break
 
-    return reversed_message  # Return the fully assembled reversed message
+    return reversed_message
 
 if __name__ == '__main__':
-    server_address = ('localhost', 12347)
+    server_address = ('localhost', 12349)
     message = input("Enter a message to send: ")
     send_message(server_address, message)
--- server_prob.py
+++ server_prob.py
@@ -1,48 +1,48 @@
 import socket
 import threading
 
-def client_handler(client_addr, server_sock, buffer_size=1024):
+def client_handler(client_addr, server_sock, buffer_size=4096):
     message_buffer = []
     expected_seq_num = 0
 
     while True:
         data, addr = server_sock.recvfrom(buffer_size)
         if addr == client_addr:
-            char, seq_num = parse_data(data)
+            message_chunk, seq_num = parse_data(data)
 
             if seq_num == expected_seq_num:
-                print(f"Received '{char}' with seq_num {seq_num} from {addr}")
-                message_buffer.append(char)
+                print(f"Received message chunk with seq_num {seq_num} from {addr}")
+                message_buffer.append(message_chunk)
                 ack = f'ACK{seq_num:02}'.encode()
                 server_sock.sendto(ack, client_addr)
 
-                if char == '\n':
-                    message = ''.join(message_buffer[:-1])
+                if message_chunk.endswith('\n'):
+                    message = ''.join(message_buffer)[:-1]  # Remove the last '\n'
                     reversed_message = reverse_and_respond(message)
 
-                    for idx, char in enumerate(reversed_message + '\n'):  # Include '\n' in enumeration
-                        send_char_with_seq(server_sock, char, expected_seq_num, client_addr)
-                        expected_seq_num += 1  # Increment after sending each char of reversed message
+                    send_in_chunks(server_sock, reversed_message, expected_seq_num, client_addr, buffer_size)
+                    break
 
-                    message_buffer = []
-                    expected_seq_num = 0
-                    break  # Exit the loop after sending the full reversed message
-                else:
-                    expected_seq_num += 1
+                expected_seq_num += 1
 
 def parse_data(data):
-    char = data[:-2].decode()
-    seq_num = int(data[-2:])
-    return char, seq_num
+    seq_num = int(data[:2])
+    message_chunk = data[2:].decode()
+    return message_chunk, seq_num
 
 def reverse_and_respond(message):
     return message[::-1]
 
-def send_char_with_seq(sock, char, seq_num, client_addr):
-    data = f'{char}{seq_num:02}'.encode()
-    sock.sendto(data, client_addr)
+def send_in_chunks(sock, message, seq_num, client_addr, buffer_size):
+    for i in range(0, len(message), buffer_size - 2):
+        chunk = message[i:i + buffer_size - 2]
+        data = f'{seq_num:02}'.encode() + chunk.encode()
+        sock.sendto(data, client_addr)
+        seq_num += 1
+    # Send end-of-message marker
+    sock.sendto(f'{seq_num:02}\n'.encode(), client_addr)
 
-def start_server(host='localhost', port=12347, buffer_size=1024):
+def start_server(host='localhost', port=12349, buffer_size=4096):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_sock:
         server_sock.bind((host, port))
         print(f"Server started at {host}:{port}")
